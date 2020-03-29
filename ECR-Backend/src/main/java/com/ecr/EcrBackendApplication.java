935eead6c3bf7a58b4e8d4aa032e59a3c4209845
annouch <55790515+annouch@users.noreply.github.com>
2020-03-29 17:35:26 +0200

    Initial commit for the project

---
+++ ECR-Backend/src/main/java/com/ecr/EcrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ecr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcrBackendApplication.class, args);
+	}
+
+}
---
+++ ECR-Backend/src/test/java/com/ecr/EcrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
