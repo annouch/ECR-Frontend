2f2370f36dd02189a42efe7bfc70f6d5e80fbf93
annouch <55790515+annouch@users.noreply.github.com>
2020-04-01 15:34:57 +0200

    Final version ECR BackEnd 1 Seule Table using-RestController

--- ECR-Backend/src/main/java/com/ecr/dao/EnfantDao.java
+++ ECR-Backend/src/main/java/com/ecr/dao/EnfantDao.java
@@ -1,10 +1,15 @@
 package com.ecr.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ecr.entities.Enfant;
+@CrossOrigin(origins="http://localhost:4200")
+@RepositoryRestController
 @Repository
 public interface EnfantDao extends JpaRepository<Enfant, Long> {
+	
 
 }
